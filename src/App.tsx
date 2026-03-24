fc0de480dddcd69f4d3b8a0054373716a73cc753
Assam-Donald247 <donald.assam@247bet.ng>
2026-03-24 17:06:11 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Authwrapper from "./components/Authwrapper/Authwrapper";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
     <div className="start-button">
+      <SpeedInsights />
       <div className="page">
         <Authwrapper />
       </div>
