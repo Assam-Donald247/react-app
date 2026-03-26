39fe6ce21171c39257c7779f65857143af95fe97
Assam-Donald247 <donald.assam@247bet.ng>
2026-03-26 13:52:12 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import Authwrapper from "./components/Authwrapper/Authwrapper";
 
 function App() {
   return (
-    <div className="start-button">
       <div className="page">
         <Authwrapper />
       </div>
-    </div>
   );
 }
 
